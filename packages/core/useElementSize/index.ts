c066b4f5644693a4f039e52734249316fdcc057a
仿生狮子 <tangnad@qq.com>
2023-01-18 14:50:49 +0800

    fix: useElementSize on SVG element

--- packages/core/useElementSize/index.ts
+++ packages/core/useElementSize/index.ts
@@ -1,4 +1,4 @@
-import { ref, watch } from 'vue-demi'
+import { computed, ref, watch } from 'vue-demi'
 import type { MaybeComputedElementRef } from '../unrefElement'
 import type { UseResizeObserverOptions } from '../useResizeObserver'
 import { useResizeObserver } from '../useResizeObserver'
@@ -23,6 +23,7 @@ export function useElementSize(
   options: UseResizeObserverOptions = {},
 ) {
   const { box = 'content-box' } = options
+  const isSVG = computed(() => unrefElement(target)?.namespaceURI?.includes('svg'))
   const width = ref(initialSize.width)
   const height = ref(initialSize.height)
 
@@ -35,14 +36,24 @@ export function useElementSize(
           ? entry.contentBoxSize
           : entry.devicePixelContentBoxSize
 
-      if (boxSize) {
-        width.value = boxSize.reduce((acc, { inlineSize }) => acc + inlineSize, 0)
-        height.value = boxSize.reduce((acc, { blockSize }) => acc + blockSize, 0)
+      if (isSVG.value) {
+        const $elem = unrefElement(target)
+        if ($elem) {
+          const styles = window.getComputedStyle($elem)
+          width.value = parseFloat(styles.width)
+          height.value = parseFloat(styles.height)
+        }
       }
       else {
-        // fallback
-        width.value = entry.contentRect.width
-        height.value = entry.contentRect.height
+        if (boxSize) {
+          width.value = boxSize.reduce((acc, { inlineSize }) => acc + inlineSize, 0)
+          height.value = boxSize.reduce((acc, { blockSize }) => acc + blockSize, 0)
+        }
+        else {
+          // fallback
+          width.value = entry.contentRect.width
+          height.value = entry.contentRect.height
+        }
       }
     },
     options,
